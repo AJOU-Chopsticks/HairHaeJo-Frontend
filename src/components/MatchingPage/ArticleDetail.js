5646924b713240687889379174c86d7caa6bee14
Daehee <mok9550@ajou.ac.kr>
2023-05-01 16:04:14 +0900

    Article Delete Modal

--- src/components/MatchingPage/ArticleDetail.js
+++ src/components/MatchingPage/ArticleDetail.js
@@ -1,9 +1,11 @@
 import React, { useState } from "react";
 import { BsFillSendFill } from "react-icons/bs";
 import ReportModal from "./ReportModal";
+import DeleteModal from "./DeleteModal";
 
 function ArticleDetail(props) {
   const [showReport, setShowReport] = useState(false);
+  const [showDelete, setShowDelete] = useState(false);
 
   const articleModifyHandler = () => {
     props.setShowDetail(false);
@@ -132,6 +134,7 @@ function ArticleDetail(props) {
             <button
               type="button"
               className="w-1/2 inline-flex items-center justify-center text-white bg-red-600 hover:bg-red-700 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-red-500 dark:hover:bg-red-600 dark:focus:ring-red-900"
+              onClick={() => setShowDelete(true)}
             >
               <svg
                 aria-hidden="true"
@@ -169,6 +172,7 @@ function ArticleDetail(props) {
         </div>
       </div>
       <ReportModal showModal={showReport} setShowModal={setShowReport} />
+      <DeleteModal showModal={showDelete} setShowModal={setShowDelete} />
     </div>
   );
 }
