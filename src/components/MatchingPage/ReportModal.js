92fe26293314d56dda897dec99edb853625d193d
Daehee <mok9550@ajou.ac.kr>
2023-05-01 16:16:20 +0900

    On Modal, scroll bug fixed

--- src/components/MatchingPage/ReportModal.js
+++ src/components/MatchingPage/ReportModal.js
@@ -20,7 +20,6 @@ function ReportModal(props) {
             type="button"
             className="absolute top-3 right-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-800 dark:hover:text-white"
             onClick={() => {
-              document.body.classList.remove("overflow-hidden");
               props.setShowModal(false);
             }}
           >
