5016a656377012ace552a62bcfd213d679fef570
ows710 <Ohwonseok@git.ajou.ac.kr>
2023-05-01 18:38:51 +0900

    add settingprofile

--- src/components/SettingDesignerprofilePage/SettingDesignerForm.js
+++ src/components/SettingDesignerprofilePage/SettingDesignerForm.js
@@ -1,9 +1,11 @@
 import React, { useState } from "react";
+import Designerportfoliolist from "../AddportfolioPage/Designerportfoliolist";
 
 function SettingDesignerForm() {
   const [type, setType] = useState(true);
   return (
     <form>
+      <br></br>
       <div class="mb-3">
         <label
           htmlFor="introduction"
@@ -112,6 +114,7 @@ function SettingDesignerForm() {
           />
         </div>
       </div>
+      <Designerportfoliolist />
       <button
         type="save"
         className="text-white bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
--- src/components/SignupPage/SignupForm.js
+++ src/components/SignupPage/SignupForm.js
@@ -3,6 +3,7 @@ import MainIcon from "../../images/MainIcon.png";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
 import { API, DEFAULT_PROFILE_IMAGE } from "../../global/Constants";
+import SettingprofilePage from "../../pages/SettingprofilePage";
 
 function SignupForm() {
   const navigation = useNavigate();
@@ -420,6 +421,7 @@ function SignupForm() {
               <button
                 disabled={loading}
                 type="submit"
+                onClick={<SettingprofilePage />}
                 className={`${
                   loading && "cursor-not-allowed"
                 } w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800`}
