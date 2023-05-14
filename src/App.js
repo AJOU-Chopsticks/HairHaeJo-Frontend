6d0b570bea61e207cfcc2948cc45f3c251f4bf02
ows710 <Ohwonseok@git.ajou.ac.kr>
2023-05-15 04:58:02 +0900

    add homepage

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ import MyPage from "./pages/MyPage";
 import ChangeprofilePage from "./pages/ChangeprofilePage";
 import LookupprofilePage from "./pages/LookupprofilePage";
 import AddportfolioPage from "./pages/AddportfolioPage";
+import HomePage from "./pages/HomePage";
 
 function App() {
   const AuthLandingPage = Auth(LandingPage, null);
@@ -26,6 +27,7 @@ function App() {
       <Header />
       <Routes>
         <Route path="/" element={<AuthLandingPage />} />
+        <Route path="/home" element={<HomePage />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/signup" element={<SignupPage />} />
         <Route path="/matching" element={<AuthMatchingPage />} />
