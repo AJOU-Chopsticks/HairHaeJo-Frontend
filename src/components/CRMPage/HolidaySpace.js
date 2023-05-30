a9c36dff8517e88ef37a8d5898b236d8b1ab50d6
ows710 <Ohwonseok@git.ajou.ac.kr>
2023-05-30 21:21:21 +0900

    add calendar

--- src/components/CRMPage/HolidaySpace.js
+++ src/components/CRMPage/HolidaySpace.js
@@ -1,31 +1,33 @@
-import React, { useState } from "react";
-import HolidayList from "./HolidayList";
-import HolidayForm from "./HolidayForm";
-import HolidayModifyForm from "./HoildayModifyForm";
+import React from "react";
+import styled from "styled-components";
+import Holiday from "./Holiday";
+import AddHoliday from "./AddHoliday";
+import EditHoliday from "./EditHoliday";
+import { useSelector } from "react-redux";
+import CalendarList from "./CalendarList";
 
 function HolidaySpace() {
-  const [showModifyForm, setShowModifyForm] = useState(false);
-  const [modifyData, setModifyData] = useState({});
-  const [reload, setReload] = useState(false);
-
+  const { isOpenEditPopup } = useSelector((state) => state);
   return (
-    <>
-      <HolidayList
-        setShowModifyForm={setShowModifyForm}
-        setModifyData={setModifyData}
-        reload={reload}
-        setReload={setReload}
-      />
-      <HolidayForm reload={reload} setReload={setReload} />
-      <HolidayModifyForm
-        showModal={showModifyForm}
-        setShowModal={setShowModifyForm}
-        modifyData={modifyData}
-        reload={reload}
-        setReload={setReload}
-      />
-    </>
+    <div>
+      <div className="p-4 mb-8 md:ml-64">
+        <CalendarList />;
+      </div>
+      <div className="p-4 mb-8 md:ml-64">
+        <Title>CALENDAR</Title>
+        <Holiday />;{/*<AddHoliday />;*/}
+        {/*<EditHoliday />;*/}
+      </div>
+    </div>
   );
 }
+
+const Title = styled.div`
+  color: gray;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 5vh;
+`;
 
 export default HolidaySpace;
